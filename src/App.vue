98bbe12f046f5375e728f4f9addb52eea097b36b
Hugo Silveira <hugo.silveira@miniclip.com>
2020-05-30 16:21:32 +0100

    Add basic business view component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
   <div id="app">
+    <h1>Power Adventure</h1>
+    <div v-if="show">
+      <BusinessItem :item="business"/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import BusinessItem from './view/components/Business.vue';
+import { Business } from './model/data/Business';
 
 @Component({
   components: {
+    BusinessItem,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  private show: boolean = false;
+  private business!: Business;
+
+  public mounted() {
+    this.business = new Business('paddle', 'paddle.png', 'Paddling', 4, 0.5, 1, Date.now());
+    this.show = true;
+  }
+}
 </script>
 
 <style>
@@ -21,5 +36,9 @@ export default class App extends Vue {}
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
---
+++ src/view/components/Business.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="business">
+    <div>{{ item.name }}</div>
+    <div>{{ item.icon }}</div>
+    <div>{{ item.capacity }}</div>
+    <div>{{ item.time / 1000 }}</div>
+    <div>{{ production }}</div>
+    <div><button type="button" @click="start">GO!</button></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+// eslint-disable-next-line no-unused-vars
+import { Business } from '../../model/data/Business';
+
+@Component
+export default class BusinessItem extends Vue {
+  @Prop() private item!: Business;
+
+  private production: number = 0;
+
+  mounted() {
+    this.production = this.item.getProduction(Date.now());
+  }
+
+  start() {
+    this.item.work(Date.now());
+
+    setTimeout(() => {
+      this.production = this.item.getProduction(Date.now());
+    }, this.item.time);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.business {
+  display: flex;
+  width: 500px;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid grey;
+  padding: 10px;
+}
+
+button {
+  border: blue;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  background: green;
+}
+</style>
