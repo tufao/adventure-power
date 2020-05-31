63f4d31b55ef52f663a972f07ddf48eab2645776
Hugo Silveira <hugo.silveira@miniclip.com>
2020-05-31 20:57:33 +0100

    Add storage proxy

---
+++ __tests__/storage.spec.ts
@@ -0,0 +1,29 @@
+import { Business } from "../src/model/data/Business";
+import { BusinessType } from "../src/model/data/BusinessType";
+import { Wallet } from "../src/model/data/Wallet";
+import { StorageProxy } from "../src/model/proxies/StorageProxy";
+import { LocalStorageMock } from "./mocks/LocalStorageMock";
+
+describe('Testing Storage operations', () => {
+    const typeLemons = new BusinessType('lemons', '', 'Lemons', 4, 0.5, 1);
+    const typeNews = new BusinessType('newspaper', '', 'Newspaper', 60, 3, 60);
+    const wallet:Wallet = new Wallet();
+
+    beforeAll(() => {
+        const lemons1 = new Business(typeLemons, Date.now());
+        const lemons2 = new Business(typeLemons, Date.now());
+        const news = new Business(typeNews, Date.now());
+        wallet.addBusiness(lemons1);
+        wallet.addBusiness(lemons2);
+        wallet.addBusiness(news);
+    })
+
+    it('Loaded wallet is equal to the saved wallet', () => {
+        const storage = new StorageProxy(new LocalStorageMock());
+        storage.saveWallet(wallet);
+
+        const loaded = storage.loadWallet();
+        expect(loaded).toMatchObject(wallet);
+    });
+
+});
---
+++ src/model/proxies/StorageProxy.ts
@@ -0,0 +1,22 @@
+import { IStorage } from './IStorage';
+import { Wallet } from '../data/Wallet';
+
+export class StorageProxy {
+    private _storage:IStorage;
+
+    constructor(storage:IStorage) {
+        this._storage = storage;
+    }
+
+    saveWallet(wallet:Wallet) {
+        const data = JSON.stringify(wallet);
+        this._storage.setItem('wallet', data);
+    }
+
+    loadWallet():Wallet {
+        const data = this._storage.getItem('wallet');
+        const wallet = Wallet.parse(JSON.parse(data));
+
+        return wallet;
+    }
+}
