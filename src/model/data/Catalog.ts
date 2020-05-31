b84004752cfd4352a81bc0dd01436bd631e490c5
Hugo Silveira <hugo.silveira@miniclip.com>
2020-05-31 16:14:00 +0100

    Add Managers support on Catalog

--- src/model/data/Catalog.ts
+++ src/model/data/Catalog.ts
@@ -1,10 +1,13 @@
 import { BusinessType } from './BusinessType';
+import { Manager } from './Manager';
 
 export class Catalog {
     private _types:Array<BusinessType>;
+    private _managers:Map<BusinessType, Manager>;
 
     constructor() {
         this._types = new Array<BusinessType>();
+        this._managers = new Map<BusinessType, Manager>();
     }
 
     get totalTypes():number {
@@ -17,5 +20,16 @@ export class Catalog {
 
     public getTypes():Array<BusinessType> {
         return this._types;
+    }
+
+    public addManager(manager:Manager) {
+        this._managers.set(manager.type, manager);
+    }
+
+    public getManager(type:BusinessType) {
+        if (this._managers.has(type)) {
+            return this._managers.get(type);
+        }
+        return null;
     }
 }
